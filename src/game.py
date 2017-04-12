91c7b2c9c940dce00dec96c67eec3ce6534a8d25
Mathieu Nachury <mathieu.nachury@outlook.com>
2017-04-12 16:39:02 +0200

    add pycache in gitignore add return grid to setGrid

--- src/game.py
+++ src/game.py
@@ -2,13 +2,15 @@ from src.constant import *
 
 class Game:
 
+    # Function to set new grid based on size
     def setGrid(self,size):
         if size > MINGRID:
             self._grid = [[0 for x in range(size)] for y in range(size)]
-            return 1
+            return self._grid
         else:
             return 0
 
+    # Return the current grid
     def getGrid(self):
         if self._grid is None:
             return 0
