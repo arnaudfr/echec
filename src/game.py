788999773eca743a094e6deb9ee8536e843f87ca
Mathieu Nachury <mathieu.nachury@outlook.com>
2017-05-23 17:08:33 +0200

    Added constructor to players

--- src/game.py
+++ src/game.py
@@ -3,11 +3,13 @@ from src.player import Player
 
 class Game:
 
+    def __init__(self):
+        self._players = []
+
     # Function to set new grid based on size
     def setGrid(self,size):
         if size > MINGRID and size < MAXGRID:
             self._grid = [[0 for x in range(size)] for y in range(size)]
-            self._players = []
             return self._grid
         else:
             return None
