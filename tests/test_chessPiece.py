34aab79c2ccc555b513ab918f226492d4dbf06c9
arnaud <arnaud.frossard@gmail.com>
2017-07-02 22:04:42 +0200

    Add invalid chess piece init test

--- tests/test_chessPiece.py
+++ tests/test_chessPiece.py
@@ -7,3 +7,7 @@ class TestChessPiece(unittest.TestCase):
     def test_CreatePiece(self):
         piece = ChessPiece([3, 1], True)
         self.assertIsNotNone(piece)
+
+    def test_CreateInvalidPiece(self):
+        piece = ChessPiece([-1000, -1000000], False)
+        self.assertIsNone(piece)
