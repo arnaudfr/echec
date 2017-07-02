4c9c81c70fadb8ac3b14c1810fcd895cd907f7e4
mnachury <mathieunachury@outlook.com>
2017-07-02 21:47:15 +0200

    Added class ChessPiece with constructor

---
+++ src/chessPiece.py
@@ -0,0 +1,11 @@
+from src.constant import *
+
+class ChessPiece:
+
+	def __init_(self,pattern,jump):
+		self.jump = jump
+		if len(pattern) != 2:
+			return None
+		if pattern[0] < 0 || pattern[1] < 0 :
+			return None
+		self.pattern = pattern
